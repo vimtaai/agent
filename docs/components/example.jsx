7a0058bb895a1b39496332b9ac56150f7eceffec
Marton Visnovitz <visnovitz.marton@gmail.com>
2025-05-11 00:44:51 +0200

    Add source code to docs examples

--- docs/components/example.jsx
+++ docs/components/example.jsx
@@ -1,15 +1,38 @@
+import { useEffect, useState } from "react";
+
 import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
+import Tabs from '@theme/Tabs';
+import TabItem from '@theme/TabItem';
+import CodeBlock from "@theme/CodeBlock";
 
 export default function Example({ model, title }) {
   const { siteConfig } = useDocusaurusContext();
-  const url = `${siteConfig.baseUrl}${model}/index.html`;
+  const modelDir = `${siteConfig.baseUrl}${model}`;
+  const htmlUrl = `${modelDir}/index.html`;
+  const jsUrl = `${modelDir}/index.js`;
+
+  const [code, setCode] = useState("");
+
+  useEffect(() => {
+    fetch(jsUrl)
+    .then(response => response.text())
+    .then(code => { setCode(code) });
+  }, [model])
 
   return (
-    <figure>
-      <iframe src={url} width="100%" height="512"></iframe>
-      <figcaption>
-        {title}
-      </figcaption>
-    </figure>
+    <Tabs>
+      <TabItem value="live" label="Live example">
+        <figure>
+          <iframe src={htmlUrl} width="100%" height="512"></iframe>
+          <figcaption>
+            {title}
+          </figcaption>
+        </figure>
+      </TabItem>
+
+      <TabItem value="code" label="Source code">
+        <CodeBlock language="js">{code}</CodeBlock>
+      </TabItem>
+    </Tabs>
   );
 };
